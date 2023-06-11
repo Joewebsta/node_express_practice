725f4d22992e2dcdf423e6fe32a89a067f9d6443
Joe Webster <joseph.scott.webster@gmail.com>
2023-06-11 19:39:34 -0400

    Add user routes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,25 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
 });
 
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ! ${process.env.PORT}`),
