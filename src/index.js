9d6f572d6932dc5a4a1dfdbc590c191dd0dabbcd
Joe Webster <joseph.scott.webster@gmail.com>
2023-06-11 20:37:10 -0400

    Create models and routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
+import routes from './routes';
+import models from './models/'
 import express from 'express';
 
 const app = express();
@@ -10,92 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
   next();
 });
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
-app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
-});
-
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.me.id,
-  };
-
-  messages[id] = message;
-
-  return res.send(message);
-});
-
-
-app.delete('/messages/:messageId', (req, res) => {
-  // What is going on here?
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = messages;
-
-  messages = otherMessages;
-
-  return res.send(message);
-});
-
-app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
-});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}`),
