793ee086c879b1c09a7c95c69d6012bfaf124597
Joe Webster <joseph.scott.webster@gmail.com>
2023-06-11 20:13:02 -0400

    Create message routes and session route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import 'dotenv/config';
 import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid';
 import express from 'express';
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
 
 app.get('/users', (req, res) => {
   return res.send('GET HTTP method on user resource');
@@ -26,6 +58,45 @@ app.delete('/users/:userId', (req, res) => {
   );
 });
 
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+
+app.delete('/messages/:messageId', (req, res) => {
+  // What is going on here?
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+
+  messages = otherMessages;
+
+  return res.send(message);
+});
+
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
+});
+
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ! ${process.env.PORT}`),
+  console.log(`Example app listening on port ${process.env.PORT}`),
 );
